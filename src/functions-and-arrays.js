97817c1cbd7bbf1b1959c7ee62166a139896bc9d
VaishnaviGanji <vaishnaviganji24@gmail.com>
2021-02-03 12:34:49 +0530

    1578196

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -61,3 +61,119 @@ const matrix = [
   [24, 55, 58, 05, 66, 73, 99, 26, 97, 17],
   [21, 36, 23, 09, 75, 00, 76, 44, 20, 45]
 ];
+
+function greatestOfTwoNumbers(a, b) {
+  if (a > b) {
+    return a;
+  }
+  return b;
+}
+
+function findScaryWord(words) {
+  if (words.length == 0)
+  return null;
+  var lengthy_word;
+  maxlength = 0;
+  if(words.length == 0)
+  return null;
+  for(var i = 0; i < words.length; i++) {
+    var curr_len = words[i].length;
+    if(curr_len > maxlength) {
+      maxlength = curr_len;
+      lengthy_word = words[i];
+    }
+  }
+  return lengthy_word;
+}
+
+function netPrice(prices) {
+  if (prices.length == 0)
+  return null;
+  var sum = 0;
+  for (var i = 0; i < prices.length; i++) {
+    sum += prices[i];
+  }
+  return sum;
+}
+
+function midPointOfLevels(levels) {
+  if (levels.length == 0)
+  return null;
+  var sum = netPrice(levels);
+  return sum / (levels.length);
+}
+
+function averageWordLength(items) {
+  if (items.length == 0)
+  return null;
+  var tot_length = 0;
+  for (var i = 0; i < items.length; i++) {
+    tot_length += items[i].length;
+  } return tot_length/items.length;
+}
+
+function uniqueArray(items) {
+  if (items.length == 0)
+  return null;
+  return items.filter((a, b) => items.indexOf(a) == b);
+}
+
+function searchElement(words, word) {
+  if (words.length == 0) {
+    return null;
+  }
+  for (var i = 0; i < words.length; i++) {
+    if (words[i] == word)
+    return true;
+  }
+  return false;
+}
+
+function howManyTimesElementRepeated(words, word) {
+  if (words.length == 0) {
+    return null;
+  }
+  var count = 0;
+  for (var i = 0; i < words.length; i++) {
+    if (words[i] == word) {
+      count++;
+    }
+  }
+  return count;
+}
+
+function maximumProduct(matrix) {
+  var maxProduct = 0;
+  var product;
+  for(var i = 0; i < 8; i++) {
+    for(var j = 0; j < 8; j++) {
+      if((j - 3) >= 0) {
+        product = matrix[i][j] * matrix[i][j - 1] * matrix[i][j - 2] * matrix[i][j - 3];
+        if(maxProduct < product)
+        maxProduct = product;
+      }
+      if((i - 3) >= 0) {
+        product = matrix[i][j] * matrix[i - 1][j] * matrix[i - 2][j] * matrix[i - 3][j];
+        if(maxProduct < product)
+        maxProduct = product;
+      }
+
+    }
+  }
+  return maxProduct;
+}
+
+function maximumProductOfDiagonals(matrix) {
+  var maxProduct = 0;
+  var product;
+  for(var i = 0; i < 8; i++) {
+    for(var j = 0; j < 8; j++) {
+      if((j - 3) >= 0 && (i - 3) >= 0) {
+        product = matrix[i][j] * matrix[i - 1][j - 1] * matrix[i - 2][j - 2] * matrix[i - 3][j - 3];
+        if(maxProduct < product)
+        maxProduct = product;
+      }
+    }
+  }
+  return maxProduct;
+}
